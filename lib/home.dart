bd2629f6902897534670a45267a37941c70cc4f2
PC-Dosen <padang.tekno.corp@gmail.com>
2023-01-04 10:13:28 +0700

    mardalius

---
+++ lib/home.dart
@@ -0,0 +1,167 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hero Widget'),
+      ),
+      body: ListView(
+        children: [
+          GestureDetector(
+            onTap: () {
+              //memperbesar gambar saat diklik
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Mobil1()));
+            },
+            child: Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  ListTile(
+                    leading: Hero(
+                      tag: 'Mobil 1',
+                      child: Image(
+                        image: AssetImage('mobil/mobil1.png'),
+                        fit: BoxFit.fill,
+                        height: 80,
+                        width: 80,
+                      ),
+                    ),
+                    title: Text('Mobil 1'),
+                    subtitle: Text('Rp. 500,000,000.-'),
+                  )
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              //memperbesar gambar saat diklik
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Mobil2()));
+            },
+            child: Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  ListTile(
+                    leading: Hero(
+                      tag: 'Mobil 2',
+                      child: Image(
+                        image: AssetImage('mobil/mobil2.jpeg'),
+                        fit: BoxFit.fill,
+                        height: 80,
+                        width: 80,
+                      ),
+                    ),
+                    title: Text('Mobil 2'),
+                    subtitle: Text('Rp. 500,000,000.-'),
+                  )
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              //memperbesar gambar saat diklik
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Mobil3()));
+            },
+            child: Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  ListTile(
+                    leading: Hero(
+                      tag: 'Mobil 3',
+                      child: Image(
+                        image: AssetImage('mobil/mobil3.jpg'),
+                        fit: BoxFit.fill,
+                        height: 80,
+                        width: 80,
+                      ),
+                    ),
+                    title: Text('Mobil 3'),
+                    subtitle: Text('Rp. 500,000,000.-'),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//hero
+class Mobil1 extends StatelessWidget {
+  const Mobil1({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mobil 1'),
+      ),
+      body: const Center(
+        child: Hero(
+          tag: 'Mobil 1',
+          child: Image(
+            image: AssetImage('mobil/mobil1.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Mobil2 extends StatelessWidget {
+  const Mobil2({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mobil 2'),
+      ),
+      body: const Center(
+        child: Hero(
+          tag: 'Mobil 2',
+          child: Image(
+            image: AssetImage('mobil/mobil2.jpeg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Mobil3 extends StatelessWidget {
+  const Mobil3({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mobil 3'),
+      ),
+      body: const Center(
+        child: Hero(
+          tag: 'Mobil 3',
+          child: Image(
+            image: AssetImage('mobil/mobil3.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
